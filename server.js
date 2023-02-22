d6010d07647f1ce34a3ebba80d9c936b7ac63eb1
MantashaZubair <zmantasha3@gmail.com>
2023-02-21 17:37:19 -0800

    code update

--- server.js
+++ server.js
@@ -19,22 +19,23 @@ const app = express()
 //middleware
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static(path.join(__dirname,'./salesapp_frontend/build')))
 
 
 //routes
 app.use("/api/v1/auth/", authRoutes)
 app.use("/api/v1/sales/", salesRoutes)
 
-//static files
-app.use(express.static(path.join(__dirname,'./salesapp_frontend/build')))
 
+//rest api
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'./salesapp_frontend/build/index.html'))
 })
 
 //port
-const PORT = process.env.PORT||8000
+const PORT = process.env.PORT||8000;
+
+//run listen
 app.listen(PORT, ()=>{
     console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white)
 })
