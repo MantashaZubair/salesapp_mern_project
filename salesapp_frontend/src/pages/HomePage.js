d1def015f992c37af766a842633d8ce93b1ef91f
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2024-08-08 12:14:02 +0530

    Update HomePage.js

--- salesapp_frontend/src/pages/HomePage.js
+++ salesapp_frontend/src/pages/HomePage.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Layout from '../component/Layout/Layout'
 import { useAuth } from '../context/auth'
+import salesapp from '../images/salesapp.jpg'
 
 const HomePage = () => {
   const[auth]=useAuth()
 
   return (
    <Layout title = "HomePage - Sales App">
-    HomePage
-    <pre>{JSON.stringify(auth,null,4)}</pre>
+     <div className='d-flex justify-content-center mt-4'>
+     <h1>Welcome  {auth.user? auth?.user.firstname:" to salesapp"}</h1>
+    
+     </div>
+     <div className='d-flex justify-content-center mt-5'>
+      <img src={salesapp} alt='salesapp' width={"350px"} height={"350px"}/>
+     </div>
+  
+   
+    {/* <pre>{JSON.stringify(auth,null,4)}</pre> */}
     </Layout>
   )
 }
